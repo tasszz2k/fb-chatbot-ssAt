0224e428763e2c4d318e9245c72224cb9e1f69b8
anhdt118 <anhdt1@ghtk.vn>
2021-03-03 23:05:57 +0700

    update code

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ def verify_fb_token(token_sent):
 
 def get_message():
     sample_responses = ["You are stunning!", "We're proud of you",
-                        "Keep on being you!", "We're greatful to know you :)"]
+                        "Keep on being you!", "We're greatful to know you :)",
+                        "Hế lô bạn"]
     # return selected item to the user
     return random.choice(sample_responses)
 
